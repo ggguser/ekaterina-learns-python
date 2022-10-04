5e0db798a1ba1ad68b0518c4648c1e826464b0ca
Aleksey Lagunov <aslagunov@avito.ru>
2022-10-04 22:41:28 +0300

    Only one solution

--- 26_replace_digits.py
+++ 26_replace_digits.py
@@ -7,31 +7,7 @@
 #8 5 12 12 15 , 0 23 15 18 12 4 !  hello, world!
 
 
-def convert_digits_to_char_0(input_digits):
-    chars = []
-    for part in input_digits.split(sep=' '):
-        if part.isdigit():
-            chars.append(chr(int(part)))
-        else:
-            chars.append(part)
-    return ''.join(chars)
-
-
-def convert_digits_to_char_1(numbers):
-    import string
-    alphabet = string.ascii_uppercase
-    numbers = numbers.split()
-    letters = ''
-    for number in numbers:
-        if number.isdigit():
-            letters += alphabet[int(number) - 1]
-        else:
-            letters += number
-
-    print(''.join(letters))
-
-
-def convert_digits_to_char_2(numbers):
+def convert_digits_to_char(numbers):
     letters = ''
     for num in numbers.split():
         if num.isdigit():
@@ -42,7 +18,4 @@ def convert_digits_to_char_2(numbers):
 
 
 if __name__ == '__main__':
-    print(convert_digits_to_char_0('8 5 12 12 15 , 0 23 15 18 12 4 !'))
-    print(convert_digits_to_char_1('8 5 12 12 15 , 0 23 15 18 12 4 !'))
-    print(convert_digits_to_char_2('8 5 12 12 15 , 0 23 15 18 12 4 !'))
-
+    print(convert_digits_to_char('8 5 12 12 15 , 0 23 15 18 12 4 !'))
